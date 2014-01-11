f96a0ac35be2e913648dcd0e109a3a90c40cae22
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2014-01-11 20:19:17 +0100

    Added a moving rectangle

--- src/Fullscreen.cpp
+++ src/Fullscreen.cpp
@@ -12,6 +12,151 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <cassert>
+#include <vector>
+
+class Color {
+public:
+    Color(float aR, float aG, float aB, float aA) :
+        mR(aR), mG(aG), mB(aB), mA(aA)
+    {
+        assert(0.0f <= mR); assert(1.0f >= mR);
+        assert(0.0f <= mG); assert(1.0f >= mG);
+        assert(0.0f <= mB); assert(1.0f >= mB);
+        assert(0.0f <= mA); assert(1.0f >= mA);
+    }
+    inline const float r() {
+        return mR;
+    }
+    inline const float g() {
+        return mG;
+    }
+    inline const float b() {
+        return mB;
+    }
+    inline const float a() {
+        return mA;
+    }
+private:
+    float mR;
+    float mG;
+    float mB;
+    float mA;
+};
+
+class vect2d {
+public:
+    vect2d(float aX, float aY) :
+        mX(aX), mY(aY)
+    {
+    }
+
+    const float x() {
+        return mX;
+    }
+    const float y() {
+        return mY;
+    }
+    const float w() {
+        return mX;
+    }
+    const float h() {
+        return mY;
+    }
+
+    void x(float aX) {
+        mX = aX;
+    }
+    void y(float aY) {
+        mY = aY;
+    }
+
+private:
+    float mX;
+    float mY;
+};
+typedef vect2d Position;
+typedef vect2d Dimension;
+typedef vect2d Speed;
+
+/**
+ * @brief Manage the animation and rendering of a rectangle
+ */
+class Rectangle {
+public:
+    typedef std::vector<Rectangle> Vector;
+
+public:
+    /**
+     * @brief Build a rectangle that must fit into the [(-1.0f, -1.0f), (1.0f, 1.0f)] screen
+     */
+    Rectangle(const Position& aPosition, const Dimension& aDimension, const Speed& aSpeed, const Color& aColor) :
+        mPosition(aPosition), mDimension(aDimension), mSpeed(aSpeed), mColor(aColor)
+    {
+        assert(0.0f <= mDimension.w()); assert(2.0f >= mDimension.w());
+        assert(0.0f <= mDimension.h()); assert(2.0f >= mDimension.h());
+        assert(-1.0f <= left());    assert(left() < 1.0f);
+        assert(-1.0f <  right());   assert(right() < 1.0f);
+        assert(-1.0f <= bottom());  assert(bottom() < 1.0f);
+        assert(-1.0f <  top());     assert(top() < 1.0f);
+    }
+
+    inline const float left() {
+        return (mPosition.x());
+    }
+    inline const float right() {
+        return (mPosition.x() + mDimension.w());
+    }
+    inline const float bottom() {
+        return (mPosition.y());
+    }
+    inline const float top() {
+        return (mPosition.y() + mDimension.h());
+    }
+    
+    /**
+     * @brief Move the rectangle according to its position, its speed, and the ellapsed time since previous frame
+     *
+     * Reposition the rectangle to the bottom/left when it reach the screen top/right (or reverse)
+     */
+    void move(float aDeltaTime) {
+        mPosition.x(mPosition.x() + (mSpeed.x() * aDeltaTime));
+        mPosition.y(mPosition.y() + (mSpeed.y() * aDeltaTime));
+        if (left() < -1.0f) {
+            mPosition.x(1.0f);
+        }
+        if (right() > 1.0f) {
+            mPosition.x(1.0f);
+        }
+        if (bottom() < -1.0f) {
+            mPosition.y(1.0f);
+        }
+        if (top() > 1.0f) {
+            mPosition.y(-1.0f);
+        }
+    }
+
+    // draw a rectangle polygon
+    void render() {
+        glBegin(GL_POLYGON);
+            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+            glVertex2f(left(), bottom());
+            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+            glVertex2f(left(), top());
+            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+            glVertex2f(right(), top());
+            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+            glVertex2f(right(), bottom());
+        glEnd();
+    }
+
+private:
+    Position    mPosition;
+    Dimension   mDimension;
+    Speed       mSpeed;
+    Color       mColor;
+};
+
 
 static void error_callback(int error, const char* description) {
     fputs(description, stderr);
@@ -24,6 +169,14 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 
 int main(void) {
+    Rectangle::Vector rectangles;
+    rectangles.push_back(Rectangle(Position(-1.0f, -0.0f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.1f, 0.0f),
+                                   Color(1.0f, 0.0f, 0.0f, 1.0f)));
+
+
+    
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit()) {
@@ -51,35 +204,35 @@ int main(void) {
     glViewport(0, 0, width, height);
     std::cout << "fullscreen (" << width << " x " << height << ")\n";
 
-    // configure projection matrix with the ratio of the windows
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    float ratio = width / (float)height;
-    glOrtho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
-
+    double currTime = glfwGetTime();
+    double prevTime = currTime;
     while (!glfwWindowShouldClose(window)) {
         // clear the buffer
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // configure model view matrix, with timed rotation
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.0f, 0.0f, 0.0f, 1.0f);
-        
-        // draw a triangle
-        glBegin(GL_TRIANGLES);
-            glColor3f(1.0f, 0.0f, 0.0f);
-            glVertex3f(-0.6f, -0.4f, 0.0f);
-            glColor3f(0.0f, 1.0f, 0.0f);
-            glVertex3f(0.6f, -0.4f, 0.0f);
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glVertex3f(0.0f, 0.6f, 0.0f);
-        glEnd();
+        currTime = glfwGetTime();
+        float ellapsedTime = static_cast<float>(currTime - prevTime);
+        std::cout << "ellapsedTime=" << ellapsedTime << std::endl;
+
+        // move the rectangles based on ellapsed time
+        for (Rectangle::Vector::iterator iRectangle  = rectangles.begin();
+                                         iRectangle != rectangles.end();
+                                       ++iRectangle) {
+            iRectangle->move(ellapsedTime);
+        }
+
+        // draw the rectangles
+        for (Rectangle::Vector::iterator iRectangle  = rectangles.begin();
+                                         iRectangle != rectangles.end();
+                                       ++iRectangle) {
+            iRectangle->render();
+        }
         
         // Swap back & front buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
+        prevTime = currTime;
     }
     glfwDestroyWindow(window);
     glfwTerminate();
