62e85634fa9eb4d564bbded3ad5831b5620c3f3e
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2014-01-11 20:55:58 +0100

    Managing horizontal overflow
     - added multiple rectangles

--- src/Fullscreen.cpp
+++ src/Fullscreen.cpp
@@ -93,10 +93,9 @@ public:
     Rectangle(const Position& aPosition, const Dimension& aDimension, const Speed& aSpeed, const Color& aColor) :
         mPosition(aPosition), mDimension(aDimension), mSpeed(aSpeed), mColor(aColor)
     {
-        assert(0.0f <= mDimension.w()); assert(2.0f >= mDimension.w());
-        assert(0.0f <= mDimension.h()); assert(2.0f >= mDimension.h());
+        assert(0.0f < mDimension.w()); assert(2.0f > mDimension.w());
+        assert(0.0f < mDimension.h()); assert(2.0f > mDimension.h());
         assert(-1.0f <= left());    assert(left() < 1.0f);
-        assert(-1.0f <  right());   assert(right() < 1.0f);
         assert(-1.0f <= bottom());  assert(bottom() < 1.0f);
         assert(-1.0f <  top());     assert(top() < 1.0f);
     }
@@ -125,8 +124,8 @@ public:
         if (left() < -1.0f) {
             mPosition.x(1.0f);
         }
-        if (right() > 1.0f) {
-            mPosition.x(1.0f);
+        if (left() > 1.0f) {
+            mPosition.x(-1.0f);
         }
         if (bottom() < -1.0f) {
             mPosition.y(1.0f);
@@ -138,16 +137,42 @@ public:
 
     // draw a rectangle polygon
     void render() {
-        glBegin(GL_POLYGON);
-            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
-            glVertex2f(left(), bottom());
-            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
-            glVertex2f(left(), top());
-            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
-            glVertex2f(right(), top());
-            glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
-            glVertex2f(right(), bottom());
-        glEnd();
+        if (right() < 1.0f) {
+            // if no horizontal overflow
+            glBegin(GL_POLYGON);
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(left(), bottom());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(left(), top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(right(), top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(right(), bottom());
+            glEnd();
+        } else {
+            // if horizontal overflow
+            float offX = right() - 2.0f;
+            glBegin(GL_POLYGON);
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(left(), bottom());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(left(), top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(1.0f, top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(1.0f, bottom());
+            glEnd();
+            glBegin(GL_POLYGON);
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(-1.0f, bottom());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(-1.0f, top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(offX, top());
+                glColor4f(mColor.r(), mColor.g(), mColor.b(), mColor.a());
+                glVertex2f(offX, bottom());
+            glEnd();
+        }
     }
 
 private:
@@ -170,12 +195,30 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(void) {
     Rectangle::Vector rectangles;
-    rectangles.push_back(Rectangle(Position(-1.0f, -0.0f),
+    rectangles.push_back(Rectangle(Position(0.3f, 0.7f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.4f, 0.0f),
+                                   Color(0.0f, 1.0f, 1.0f, 1.0f)));
+    rectangles.push_back(Rectangle(Position(0.0f, 0.4f),
                                    Dimension(1.0f, 0.2f),
                                    Speed(0.1f, 0.0f),
+                                   Color(0.0f, 0.0f, 1.0f, 1.0f)));
+    rectangles.push_back(Rectangle(Position(-0.6f, 0.1f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.6f, 0.0f),
+                                   Color(0.0f, 1.0f, 0.0f, 1.0f)));
+    rectangles.push_back(Rectangle(Position(-1.0f, -0.2f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.5f, 0.0f),
                                    Color(1.0f, 0.0f, 0.0f, 1.0f)));
-
-
+    rectangles.push_back(Rectangle(Position(-1.0f, -0.5f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.2f, 0.0f),
+                                   Color(1.0f, 0.0f, 1.0f, 1.0f)));
+    rectangles.push_back(Rectangle(Position(-1.0f, -0.8f),
+                                   Dimension(1.0f, 0.2f),
+                                   Speed(0.3f, 0.0f),
+                                   Color(1.0f, 1.0f, 0.0f, 1.0f)));
     
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
@@ -183,7 +226,11 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
+    // Ask for SRGB linear to gama conversion buffer
     glfwWindowHint(GLFW_SRGB_CAPABLE, GL_TRUE);
+    
+    // Ask for vertical synch (not working)
+    // glfwSwapInterval(1);
 
     // get the current Desktop screen resolution and colour depth
     const GLFWvidmode* pCurrentVideoMod = glfwGetVideoMode(glfwGetPrimaryMonitor());
